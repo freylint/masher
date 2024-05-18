cd03b0f6288540b0b5a88b9d3e27657f8d8e4f68
lmpriestley <lmpriestley@outlook.com>
2024-05-18 19:08:48 -0400

    Port everything but the Timer registers to the ATTINY84

--- projects/scripts/flash.py
+++ projects/scripts/flash.py
@@ -3,9 +3,9 @@ import subprocess
 
 PROGRAMMER = "usbasp"
 TARGET = "t84"
-TARGET_CLOCK_MHZ = "20"
-BIN = "target/avr-attiny84/release/semasher.elf"
+TARGET_CLOCK_MHZ = "12"
+BIN = "projects/firmware/main.hex"
 # This Should work correctly, if not let me know -Sam
-command = f"avrdude -c {PROGRAMMER} -p {TARGET} -B{TARGET_CLOCK_MHZ}-U flash:w:{BIN}"
+command = f"avrdude -c {PROGRAMMER} -p {TARGET} -B {TARGET_CLOCK_MHZ} -U flash:w:{BIN}"
 result = subprocess.check_output(command, shell=True, text=True)
 print(result)
